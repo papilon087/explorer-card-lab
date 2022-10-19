bbfb0dc332d56a11392dfef274098bda50a21a83
tabaco <tabaco@pop-os.localdomain>
2022-10-19 15:34:22 -0300

    aula 02

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import "./css/index.css"
+import IMask from 'imask'
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
 const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
 
-ccBgColor01.setAttribute("fill", "green")
-ccBgColor02.setAttribute("fill", "blue")
+ccBgColor01.setAttribute("fill", "gray")
+ccBgColor02.setAttribute("fill", "black")
 
 
 function setCardType(type) {
@@ -21,3 +22,91 @@ function setCardType(type) {
 }
 
 globalThis.setCardType = setCardType
+
+// security code 
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+    mask: "0000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+// expiration code 
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDataPattern = {
+    mask: "MM{/}YY",
+    blocks: {
+      YY: {
+        mask: IMask.MaskedRange,
+        from: String(new Date().getFullYear()).slice(2),
+        to: String(new Date().getFullYear() + 10).slice(2)
+      },
+      MM: {
+        mask: IMask.MaskedRange,
+        from: 1,
+        to: 12,
+      },
+    },
+}
+
+const expirationDataMasked = IMask(expirationDate, expirationDataPattern)
+
+const cardNumber = document.querySelector("#card-number")
+const cardNumberPattern = {
+    mask: [
+        {
+            mask: '0000 000000 00000',
+            regex: /^3[47]\d{0,13}/,
+            cardtype: 'american express'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: /^(?:6011|65\d{0,2}|64[4-9]\d?)\d{0,12}/,
+            cardtype: 'discover'
+        },
+        {
+            mask: '0000 000000 0000',
+            regex: /^3(?:0([0-5]|9)|[689]\d?)\d{0,11}/,
+            cardtype: 'diners'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: /^(5[1-5]\d{0,2}|22[2-9]\d{0,1}|2[3-7]\d{0,2})\d{0,12}/,
+            cardtype: 'mastercard'
+        },
+        {
+            mask: '0000 000000 00000',
+            regex: /^(?:2131|1800)\d{0,11}/,
+            cardtype: 'jcb15'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: /^(?:35\d{0,2})\d{0,12}/,
+            cardtype: 'jcb'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: /^(?:5[0678]\d{0,2}|6304|67\d{0,2})\d{0,12}/,
+            cardtype: 'maestro'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: /^4\d{0,15}/,
+            cardtype: 'visa'
+        },
+        {
+            mask: '0000 0000 0000 0000',
+            regex: /^62\d{0,14}/,
+            cardtype: 'unionpay'
+        },
+    ],
+    dispatch: function (appended, dynamicMasked) {
+       const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+       const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+          return number.match(item.regex)
+       })
+    
+       return foundMask
+    },
+}
+
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
